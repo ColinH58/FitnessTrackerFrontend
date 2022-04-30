2c13d45b57403db1fbd3a6d8092dd9803bc52417
Colin Hoolwerf <choolwerf@gmail.com>
2022-04-30 13:17:25 -0500

    working on some quick and dirty CSS

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import {
   HomePage,
   Activities,
   Routines,
+  MyRoutines,
+  Login,
+  Register,
 } from "./components";
 
 const App = () => {
   return (
     <div className="App">
+      <nav className="navbar">
+        <Login />
+        <Register />
+      </nav>
       <HomePage />
       <Activities />
       <Routines />
+      <MyRoutines />
     </div>
   );
 };
---
+++ src/components/MyRoutines.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyRoutines = () => {
+  return (
+    <div>
+        <h3>The user ROUTINE ACTIVITIES here</h3>
+    </div>
+  )
+};
+
+export default MyRoutines;
