a08f64d06c8dd10e6ac77b5d23ef1278d144a8ba
Colin Hoolwerf <choolwerf@gmail.com>
2022-04-30 17:39:51 -0500

    minor tweaks, now ready to go

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,6 @@ const HomePage = () => {
   return (
     <div className="Components">
       <h2>Welcome to your Personal Fitness Tracker!</h2>
-      <p>testing something out</p>
     </div>
   );
 };
