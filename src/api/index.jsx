37ad99524faa7cd00a7b33517085eadbaf0d5fce
Colin Hoolwerf <choolwerf@gmail.com>
2022-05-07 11:12:59 -0500

    added routes from Victoria's branch

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -47,19 +47,20 @@ export const getRoutines = async () => {
   return json;
 };
 
-export const getMyRoutines = async () => {
-  const url =
-  "https://fast-plateau-20949.herokuapp.com/api/users/myroutines"; //THIS ONE IS WRONG, USER ID + ROUTINES, NOT 'MyRoutines'
-  const token = localStorage.getItem("token");
-  const response = await fetch(url, {
-      method: "GET",
+export const getMyRoutines = async (username) => {
+  return await fetch(
+    `https://fast-plateau-20949.herokuapp.com/api/users/${username}/routines`,
+    {
       headers: {
         "Content-Type": "application/json",
-        'Authorization': `Bearer ${token}`
       },
-  });
-  const json = await response.json();
-  return json;
+    }
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(console.error);
 };
 
 export const accountLogin = async (username, password) => {
@@ -87,7 +88,6 @@ export const accountLogin = async (username, password) => {
 };
 
 export const accountCreation = async (username, password) => {
-  console.log(username, password, "username and password")
   await fetch(
     "https://fast-plateau-20949.herokuapp.com/api/users/register",
     {
@@ -105,6 +105,73 @@ export const accountCreation = async (username, password) => {
     .then((result) => {
       console.log(result, "the result");
       localStorage.setItem("token", result.token);
+    })
+    .catch(console.error);
+};
+
+export const getMe = async () => {
+  const token = localStorage.getItem("token");
+  return await fetch("https://fast-plateau-20949.herokuapp.com/api/users/me", {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(console.error);
+};
+
+export const newRoutine = async (name, goal) => {
+  return await fetch("https://fast-plateau-20949.herokuapp.com/api/routines", {
+    method: "POST",
+    token: localStorage.getItem("token"),
+    body: JSON.stringify({
+      name: name,
+      goal: goal,
+      isPublic: true,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(console.error);
+};
+
+export const updateRoutine = async (routineId, name, goal) => {
+  return await fetch(
+    `https://fast-plateau-20949.herokuapp.com/api/routines/${routineId}`,
+    {
+      method: "PATCH",
+      token: localStorage.getItem("token"),
+      body: JSON.stringify({
+        name: name,
+        goal: goal,
+      }),
+    }
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(console.error);
+};
+
+export const deleteRoutine = async (routineId) => {
+  const token = localStorage.getItem("token");
+  fetch(`https://fast-plateau-20949.herokuapp.com/api/routines/${routineId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
     })
     .catch(console.error);
 };
